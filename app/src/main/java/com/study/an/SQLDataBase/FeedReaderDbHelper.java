f5bda3f9c15dde457c5dcd489f9055801fec938c
rockan007 <rockan007@gmail.com>
2016-07-07 17:28:28 +0800

    okHttp啥的

--- app/src/main/java/com/study/an/SQLDataBase/FeedReaderDbHelper.java
+++ app/src/main/java/com/study/an/SQLDataBase/FeedReaderDbHelper.java
@@ -3,30 +3,69 @@ package com.study.an.SQLDataBase;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by admin on 2016/7/1.
  */
-//public class FeedReaderDbHelper extends SQLiteOpenHelper {
-//    // If you change the database schema, you must increment the database version.
-//    public static final int DATABASE_VERSION = 1;
-//    public static final String DATABASE_NAME = "FeedReader.db";
-//
-//    public FeedReaderDbHelper(Context context) {
-//        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-//    }
-//
-//    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//        db.execSQL(SQL_CREATE_ENTRIES);
-//    }
-//
-//    @Override
-//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL(SQL_DELETE_ENTRIES);
-//        onCreate(db);
-//    }
-//    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        onUpgrade(db, oldVersion, newVersion);
-//    }
-//}
+public class FeedReaderDbHelper extends SQLiteOpenHelper {
+    private static FeedReaderDbHelper newInstance;
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE="INTEGER";
+
+    public static FeedReaderDbHelper getNewInstance(Context context) {
+        if (newInstance == null) {
+            newInstance = new FeedReaderDbHelper(context);
+        }
+        return newInstance;
+    }
+
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE"+FeedEntry.TABLE_NAME+"("+
+                    FeedEntry._ID+"INTEGER PRIMARY KEY"+COMMA_SEP+
+                    FeedEntry.COLUMN_NAME_ENTRY_ID+INT_TYPE+COMMA_SEP+
+                    FeedEntry.COLUMN_NAME_NAME+TEXT_TYPE+COMMA_SEP+
+                    FeedEntry.COLUMN_NAME_AGE+INT_TYPE+COMMA_SEP+
+                    FeedEntry.COLUMN_NAME_SEX+TEXT_TYPE+COMMA_SEP+
+                    FeedEntry.COLUMN_NAME_EDU+TEXT_TYPE+
+                    " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    public FeedReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    /**
+     * Inner class that defines the table contents
+     */
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "Id";
+        public static final String COLUMN_NAME_NAME = "姓名";
+        public static final String COLUMN_NAME_AGE = "年龄";
+        public static final String COLUMN_NAME_SEX = "性别";
+        public static final String COLUMN_NAME_EDU = "学历";
+    }
+}
--- app/src/main/java/com/study/an/SQLDataBase/SQLMainActivity.java
+++ app/src/main/java/com/study/an/SQLDataBase/SQLMainActivity.java
@@ -1,10 +1,63 @@
 package com.study.an.SQLDataBase;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by admin on 2016/7/1.
  */
-public class SQLMainActivity  extends AppCompatActivity {
+public class SQLMainActivity extends AppCompatActivity {
+    FeedReaderDbHelper mDbHelper;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDbHelper=FeedReaderDbHelper.getNewInstance(this);
+        insertData();
+        updateData();
+    }
+    //写 insert
+    private void insertData(){
+        ContentValues values=new ContentValues();
+        SQLiteDatabase database=mDbHelper.getWritableDatabase();
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_NAME,"AN");
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_AGE,18);
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_ENTRY_ID,007);
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_SEX,"男");
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_EDU,"小学");
+        database.insert(FeedReaderDbHelper.FeedEntry.TABLE_NAME,null,values);
+        database.close();
+    }
+    //改 update
+    private void updateData(){
+        ContentValues values=new ContentValues();
+        values.put(FeedReaderDbHelper.FeedEntry.COLUMN_NAME_AGE,16);
+        SQLiteDatabase database=mDbHelper.getWritableDatabase();
+        String selection= FeedReaderDbHelper.FeedEntry.COLUMN_NAME_NAME+" LIKE ?";
+        String[] selectionArgs={"AN"};
+        database.update(FeedReaderDbHelper.FeedEntry.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+        database.close();
+    }
+    //删除 delete
+    private void deleteData(){
+        SQLiteDatabase database=mDbHelper.getWritableDatabase();
+        String selection= FeedReaderDbHelper.FeedEntry.COLUMN_NAME_NAME+" LIKE ?";
+        String[] selectionArgs={"AN"};
+        database.delete(FeedReaderDbHelper.FeedEntry.TABLE_NAME,
+                selection,
+                selectionArgs);
+        database.close();
+    }
+    //查询 query
+    private void queryData(){
+        SQLiteDatabase database=mDbHelper.getReadableDatabase();
+        Cursor cursor=database.query(false, FeedReaderDbHelper.FeedEntry.TABLE_NAME,null,null,null,null,null,null,null);
 
+    }
 }
